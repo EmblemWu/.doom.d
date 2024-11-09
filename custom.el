52e2a1ab6d1342524bed9d8d455acaf99852cd53
Embelm Wu <emblemwu@outlook.com>
2024-11-09 21:42:05 +0800

    init

---
+++ custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sage-shell:use-prompt-toolkit nil)
+ '(sage-shell:use-simple-prompt t)
+ '(sage-shell:set-ipython-version-on-startup nil)
+ '(sage-shell:check-ipython-version-on-startup nil)
+
+ '(custom-safe-themes
+   '("3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "3d47380bf5aa650e7b8e049e7ae54cdada54d0637e7bac39e4cc6afb44e8463b" "e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "d6844d1e698d76ef048a53cefe713dbbe3af43a1362de81cdd3aefa3711eae0d" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "c4063322b5011829f7fdd7509979b5823e8eea2abf1fe5572ec4b7af1dd78519" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "1bddd01e6851f5c4336f7d16c56934513d41cc3d0233863760d1798e74809b4b" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "c2aeb1bd4aa80f1e4f95746bda040aafb78b1808de07d340007ba898efa484f5" "b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4" "84b14a0a41bb2728568d40c545280dbe7d6891221e7fbe7c2b1c54a3f5959289" "1f1b545575c81b967879a5dddc878783e6ebcca764e4916a270f9474215289e5" "613aedadd3b9e2554f39afe760708fc3285bf594f6447822dd29f947f0775d6c" "da186cce19b5aed3f6a2316845583dbee76aea9255ea0da857d1c058ff003546" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "846b3dc12d774794861d81d7d2dcdb9645f82423565bfb4dad01204fa322dbd5" default))
+ '(elfeed-feeds '("https://www.gcores.com/rss"))
+ '(package-selected-packages '(osx-dictionary)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  '(org-level-1 ((t (:inherit outline-1 :height 1.5  :foreground "#8B6969"))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.4  :foreground "#66CDAA"))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.3  :foreground "#4B0082"))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.2  :foreground "#708090"))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.1  :foreground "#CD853F"))))
+  '(org-level-6 ((t (:inherit outline-6 :height 1.1  :foreground "#A6E22E"))))
+  '(org-level-7 ((t (:inherit outline-7 :height 1.1  :foreground "#F92672"))))
+  '(org-level-8 ((t (:inherit outline-8 :height 1.1  :foreground "#66D9EF"))))
+
+ )
